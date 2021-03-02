d569bc5b8f7fad4203dc2e88613df9e0cff344b1
gotdibbs <gotdibbs@outlook.com>
2021-03-02 23:34:59 +0000

    DNS over HTTPS, cleanup update script

--- update.sh
+++ update.sh
@@ -4,11 +4,9 @@ sudo apt -yf dist-upgrade
 sudo apt-get -y --purge autoremove
 sudo apt-get autoclean
 
-sudo docker pull portainer/portainer-ce:latest
-sudo docker pull pihole/pihole:latest
-sudo docker pull oznu/homebridge:latest
-sudo docker pull ghcr.io/linuxserver/unifi-controller:latest
-sudo docker image prune
+docker-compose pull
 
 docker-compose down
 docker-compose up -d
+
+sudo docker image prune -a -f
